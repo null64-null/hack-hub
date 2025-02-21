5c0766ac090150ec2e855e2749c0fdf178dc53d3
null64-null <hay.tech64@gmail.com>
2025-02-21 12:45:09 +0900

    初期化

--- .open-next/env/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_API_URL":"http://localhost:8787"};
-export const development = {"NEXT_PUBLIC_API_URL":"http://localhost:8787"};
-export const test = {};
--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/yudaihamashima/development/hack-hub/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
